e5d925f1a734ec282af50273d8f2fc84b116eb1a
tkomar2703 <tkomar@gmx.at>
2014-03-11 21:51:38 +0100

    User Koordinaten Abfrage bei "karte.html"
    Auskommentiert: Erster Versuch der Koordinatenabfrage per Mausklick...
    noch nicht erfolgreich
    
    "test.html": Code für Koordinatenabfrage

--- menue.php
+++ menue.php
@@ -12,9 +12,17 @@
 		
 	<?php 
 		set_time_limit(0); // ansonsten nach 30 Sekunden abbruch
-		$rueck = " 2>&1"; // Rückgabewert der Tools umleiten
-		$dir = "D:\Bakk\osmosis-latest\bin"; 
-		$befehl = '"'.$dir.'\osmosis" --read-pbf file="'.$dir.'\australia-oceania-latest.osm.pbf" --bounding-polygon file='.$dir.'"\country.poly" --write-xml file='.$dir.'"\australia.osm"'.$rueck;
+		//$rueck = " 2>&1"; // Rückgabewert der Tools umleiten
+		$dir = 'D:/Bakk/osmosis-latest/bin';
+		//$befehl = '"'.$dir.'\osmosis" --read-pbf file="'.$dir.'\australia-oceania-latest.osm.pbf" --bounding-polygon file='.$dir.'"\country.poly" --write-xml file='.$dir.'"\australia.osm"'.$rueck;
+		$befehl = sprintf(
+			'%s --read-pbf file=%s --bounding-polygon file=%s --write-xml file=%s 2>&1', // "2>&1" Rückgabewert der Tools anzeigen
+			escapeshellarg($dir . '/osmosis'),
+			escapeshellarg($dir . '/australia-oceania-latest.osm.pbf'),
+			escapeshellarg($dir . '/country.poly'),
+			escapeshellarg($dir . '/australia.osm')
+			);
+		
 		//echo "$befehl";
 		//$output = shell_exec('"D:\Bakk\osmosis-latest\bin\osmosis" --read-pbf file="D:\Bakk\osmosis-latest\bin\australia-oceania-latest.osm.pbf" --bounding-polygon file="D:\Bakk\osmosis-latest\bin\country.poly" --write-xml file="D:\Bakk\osmosis-latest\bin\australia.osm" 2>&1');  
 		$output = shell_exec($befehl);  
