a7314d9298f16b2491593289fd4d1d93029af7d9
tkomar2703 <tkomar@gmx.at>
2014-04-29 12:43:53 +0200

    Bounding Polygon & GeoJSON
    Bounding Polygon ist nun vollständig implementiert.
    Downloadoption für GeoJSON wurde hinzugefügt.

--- ausfuehrung.php
+++ ausfuehrung.php
@@ -32,7 +32,7 @@ function zippen($sourcePath, $zip) {
 
 
 // Pfade auf Variablen schreiben
-$dir_osm = 'D:/Bakk/osmosis-latest/bin';
+$dir_osmosis = 'D:/Bakk/osmosis/bin';
 $dir_daten = 'D:/Bakk/Daten';
 $dir_wget = 'D:/Bakk/GnuWin32/bin';
 $dir_convert_filter = 'D:/Bakk';
@@ -45,14 +45,11 @@ $lat1 = $_GET["lat1"];
 $lon2 = $_GET["lon2"];
 $lat2 = $_GET["lat2"];
 
-if (empty($_GET["poly_koord"])) { echo "idiot";}
-else {
-$poly_koord = $_GET["poly_koord"];
-echo $poly_koord;
-$datei_handle_poly=fopen($dir_convert_filter."/poly_koord.poly","w+"); // .xml für Overpass-API
-fwrite($datei_handle_poly,"polygon\r\n1\r\n".$poly_koord."END\r\nEND");
-fclose($datei_handle_poly);
+
+if (isset($_GET['ausw_bounding'])) {// Bounding Box oder Polygon
+	$bounding_art = $_GET['ausw_bounding'];
 }
+
 // Überprüfen ob alle notwendigen Variablen übergeben wurden
 if (empty($_GET["lon1"]) || empty($_GET["lon2"]) || empty($_GET["lat1"]) || empty($_GET["lat2"])) {		
 	echo "Keine oder nicht alle Eckpunkte f&uuml;r die Bounding Box gew&#xE4;hlt";
@@ -141,7 +138,7 @@ $befehl_overpass = sprintf(
 	'%s '.$server.'interpreter --ignore-length --post-file=%s -O %s',
 	escapeshellarg($dir_wget . '/wget'),
 	escapeshellarg($dir_daten . '/overpass_api.xml'),
-	escapeshellarg($dir_daten . '/output_overpass.osm.pbf')
+	escapeshellarg($dir_daten . '/output_overpass.osm')
 	);
 
 $output_overpass = shell_exec($befehl_overpass);  // Export mittels wget/Overpass-AP	
@@ -160,12 +157,32 @@ $output_overpass = shell_exec($befehl_overpass);  // Export mittels wget/Overpas
 		//$output = shell_exec('"D:\Bakk\osmosis-latest\bin\osmosis" --read-pbf file="D:\Bakk\osmosis-latest\bin\australia-oceania-latest.osm.pbf" --bounding-polygon file="D:\Bakk\osmosis-latest\bin\country.poly" --write-xml file="D:\Bakk\osmosis-latest\bin\australia.osm" 2>&1');  
 	
 		//echo "$befehl_overpass";
+
+if (strcmp($bounding_art,'ausw_bpoly') == 0) // wenn Bounding Polygon -> Polygon File erstellen und mittels Osmosis ausschneiden
+{
+	$poly_koord = $_GET["poly_koord"];
+	$datei_handle_poly=fopen($dir_daten."/poly_koord.poly","w+"); // .xml für Overpass-API
+	fwrite($datei_handle_poly,"polygon\r\n1\r\n".$poly_koord."END\r\nEND");
+	fclose($datei_handle_poly);
+	
+	$befehl_osmosis = sprintf(
+	'%s --read-xml file=%s --bounding-polygon file=%s completeWays=yes --write-xml file=%s',
+	escapeshellarg($dir_osmosis . '/osmosis'),
+	escapeshellarg($dir_daten . '/output_overpass.osm'),
+	escapeshellarg($dir_daten . '/poly_koord.poly'),
+	escapeshellarg($dir_daten . '/output_osmosis.osm')
+	);
+
+$output_osmosis = shell_exec($befehl_osmosis);
+$file_convert = '/output_osmosis.osm';
+}
 		
-		
+else {$file_convert = '/output_overpass.osm';}
+
 $befehl_convert_to_o5m = sprintf(
 	'%s %s -o=%s',
 	escapeshellarg($dir_convert_filter . '/osmconvert'),
-	escapeshellarg($dir_daten . '/output_overpass.osm.pbf'),
+	escapeshellarg($dir_daten . $file_convert),
 	escapeshellarg($dir_daten . '/output_convert.o5m')
 	//escapeshellarg($dir_daten . '/output_convert.osm')
 );
@@ -205,17 +222,24 @@ $output_convert = shell_exec("$befehl_filter $rueck");
 		//echo "$output_convert";
 	//}
 	
-$befehl1_ogr2ogr = sprintf(
+$befehl_ogr2ogr = sprintf(
 	"%s par",
 	escapeshellarg($dir_ogr2ogr . '/SDKShell.bat')
 );
 			
-$befehl2_ogr2ogr = sprintf(
+$befehl_ogr2ogr_shape = sprintf(
 	"SET par=ogr2ogr -f \"ESRI Shapefile\" %s %s ".$shapes." --config OSM_USE_CUSTOM_INDEXING NO",
 	escapeshellarg($dir_shape . "/shape"),
 	escapeshellarg($dir_daten . '/output_filter.osm')
 );
-		
+
+$befehl_ogr2ogr_json = sprintf(
+	"SET par=ogr2ogr -f \"GeoJSON\" %s %s ".$shapes." --config OSM_USE_CUSTOM_INDEXING NO",
+	escapeshellarg($dir_daten . "/output_geojson.geojson"),
+	escapeshellarg($dir_daten . '/output_filter.osm')
+);
+	
+
 		//echo $befehl_ogr2ogr;
 		//echo $output_ogr2ogr;
 
@@ -243,7 +267,7 @@ switch ($typ_ausw) { // welche Downloadoption wurde gewählt
 		if ($zip->open($filename, ZIPARCHIVE::CREATE)!==TRUE) {
 			exit("cannot open <$filename>\n");
 		}
-		$output_ogr2ogr = shell_exec("$befehl2_ogr2ogr && $befehl1_ogr2ogr");
+		$output_ogr2ogr_shape = shell_exec("$befehl_ogr2ogr_shape && $befehl_ogr2ogr");
 		zippen($dir_shape, $zip);
 		$zip->close();
 		header ("Location: download.php?loeschen=".$loeschen); 
@@ -263,11 +287,58 @@ switch ($typ_ausw) { // welche Downloadoption wurde gewählt
 			exit("cannot open <$filename>\n");
 		}
 		$zip->addFile($dir_daten . "/output_filter.osm","output_filter.osm");
-		$output_ogr2ogr = shell_exec("$befehl2_ogr2ogr && $befehl1_ogr2ogr");
+		$output_ogr2ogr_shape = shell_exec("$befehl_ogr2ogr_shape && $befehl_ogr2ogr");
 		zippen($dir_shape, $zip);
 		$zip->close();
 		header ("Location: download.php?loeschen=".$loeschen);			
 		break;
+		
+	case "json":	
+		if ($zip->open($filename, ZIPARCHIVE::CREATE)!==TRUE) {
+			exit("cannot open <$filename>\n");
+		}
+		$output_ogr2ogr_geojson = shell_exec("$befehl_ogr2ogr_json && $befehl_ogr2ogr");
+		$zip->addFile($dir_daten . "/output_geojson.geojson","output_geojson.geojson");
+		$zip->close();
+		header ("Location: download.php?loeschen=".$loeschen); 
+		break;
+		
+	case "osm json":	
+		if ($zip->open($filename, ZIPARCHIVE::CREATE)!==TRUE) {
+			exit("cannot open <$filename>\n");
+		}
+		$output_ogr2ogr_geojson = shell_exec("$befehl_ogr2ogr_json && $befehl_ogr2ogr");
+		$zip->addFile($dir_daten . "/output_filter.osm","output_filter.osm");
+		$zip->addFile($dir_daten . "/output_geojson.geojson","output_geojson.geojson");
+		$zip->close();
+		header ("Location: download.php?loeschen=".$loeschen); 
+		break;
+		
+	case "shp json":	
+		if ($zip->open($filename, ZIPARCHIVE::CREATE)!==TRUE) {
+			exit("cannot open <$filename>\n");
+		}
+		$output_ogr2ogr_geojson = shell_exec("$befehl_ogr2ogr_json && $befehl_ogr2ogr");
+		$zip->addFile($dir_daten . "/output_geojson.geojson","output_geojson.geojson");
+		$output_ogr2ogr_shape = shell_exec("$befehl_ogr2ogr_shape && $befehl_ogr2ogr");
+		zippen($dir_shape, $zip);
+		$zip->close();
+		header ("Location: download.php?loeschen=".$loeschen); 
+		break;
+		
+	case "shp osm json":	
+		if ($zip->open($filename, ZIPARCHIVE::CREATE)!==TRUE) {
+			exit("cannot open <$filename>\n");
+		}
+		$output_ogr2ogr_geojson = shell_exec("$befehl_ogr2ogr_json && $befehl_ogr2ogr");
+		$zip->addFile($dir_daten . "/output_filter.osm","output_filter.osm");
+		$zip->addFile($dir_daten . "/output_geojson.geojson","output_geojson.geojson");
+		$output_ogr2ogr_shape = shell_exec("$befehl_ogr2ogr_shape && $befehl_ogr2ogr");
+		zippen($dir_shape, $zip);
+		$zip->close();
+		header ("Location: download.php?loeschen=".$loeschen); 
+		break;	
+		
 }
 ?>
 	
